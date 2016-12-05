169e309b1f31774002164eb77a797b6625bf85a9
cychan <13926262295@139.COM>
2016-12-05 18:07:24 +0800

    popen read()

--- listen.py
+++ listen.py
@@ -9,13 +9,13 @@ def mytime():
  otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
  return otherStyleTime
 while 1:
-    time.sleep(5)
+    time.sleep(1)
     m=mytime()
     java_pid_nu=os.popen('ps aux | grep java | grep -v grep | wc -l')
-    if java_pid_nu==1:
+    if java_pid_nu.read() == '2':
         java_sub_process=('ps aux | grep java | grep -v grep |awk \'{print $2}\' | xargs pmap -x | wc -l')
         print r'java_process is fine'
-        if java_sub_process>1000:
+        if java_sub_process.read() > 1000:
             mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_sub_process','异常')
             print r'java_sub_process is not good'
         else:
@@ -24,3 +24,4 @@ while 1:
     else:
         mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_process','异常')
         print r'java_process is not good'
+        print java_pid_nu.read()
