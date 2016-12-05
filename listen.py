ddebafaae6ca50a264e7f57be4e1f6c3e4be6318
cychan <13926262295@139.COM>
2016-12-05 17:41:23 +0800

    java process check

--- listen.py
+++ listen.py
@@ -12,11 +12,11 @@ while 1:
     time.sleep(5)
     m=mytime()
     java_pid_nu=os.system('ps aux | grep java | grep -v grep | wc -l')
-    if java_pid_nu!=1:
-        print m+'err'
-    else:
+    if java_pid_nu==1:
         java_sub_process=('ps aux | grep java | grep -v grep |awk \'{print $2}\' | xargs pmap -x | wc -l')
         if java_sub_process>1000:
             mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_sub_process','异常','smtp.mxhichina.com','cy.chen@networkgrand.com','123qwe!@#QWE')
         else:
             mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_sub_process','正常','smtp.mxhichina.com','cy.chen@networkgrand.com','123qwe!@#QWE')
+    else:
+        mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_process','异常','smtp.mxhichina.com','cy.chen@networkgrand.com','123qwe!@#QWE')
