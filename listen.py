372a7e738e20bde56842528d1bdf361354ae2f36
cychan <13926262295@139.COM>
2016-12-05 17:55:38 +0800

    print imformation

--- listen.py
+++ listen.py
@@ -14,9 +14,13 @@ while 1:
     java_pid_nu=os.popen('ps aux | grep java | grep -v grep | wc -l')
     if java_pid_nu==1:
         java_sub_process=('ps aux | grep java | grep -v grep |awk \'{print $2}\' | xargs pmap -x | wc -l')
+        print r'java_process is fine'
         if java_sub_process>1000:
             mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_sub_process','异常')
+            print r'java_sub_process is not good'
         else:
             mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_sub_process','正常')
+            print r'java_sub_process is fine'
     else:
         mail.pysendmail('cy.chen@networkgrand.com','cy.chen@networkgrand.com','java_process','异常')
+        print r'java_process is not good'
